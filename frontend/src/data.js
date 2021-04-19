33f76bc73c8b696d2de73a5c2e25a8449c6cd029
Pankaj <engg.pankaj98@gmail.com>
2021-04-20 01:57:45 +0530

    added till react app

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+export default{
+    products:[{
+       name: 'Joggers',
+       category: 'Joggers and TrackPants',
+       image: '/images/d1.jpeg',
+       price:  999,
+       brand : 'Nike',
+       rating: 4.3,
+       numReviews: 34
+    },
+    {
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price:  1499,
+        brand : 'Allen Solly',
+        rating: 4.5,
+        numReviews: 49
+     },
+     {
+        name: 'Chinos',
+        category: 'Pants',
+        image: '/images/d1.jpeg',
+        price:  1299,
+        brand : 'Hrx',
+        rating: 4.1,
+        numReviews: 78
+     },
+     {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpeg',
+        price:  1999,
+        brand : 'Blackberry',
+        rating: 4.8,
+        numReviews: 94
+     }
+]
+}
